2ca771a8ed3475b7d638348bfe5b69ab5e9937f1
insolita <webmaster100500@ya.ru>
2018-05-16 16:30:44 +0800

    fix aliases

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -3,6 +3,7 @@ define('YII_ENV', 'test');
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+Yii::setAlias('@vendor', __DIR__.'/../vendor');
 Yii::setAlias('@bower', '@vendor/bower-asset');
 Yii::setAlias('@npm', '@vendor/npm-asset');
 require __DIR__ .'/../vendor/autoload.php';
